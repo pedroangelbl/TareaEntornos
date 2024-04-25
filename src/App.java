2b9b3a9ec18dbf046f3ef6484711580336da089f
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2024-04-25 15:36:06 +0200

    feature2_2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esto es el feature2_1");
+        System.out.println("Esto es el feature2_2");
     }
 }
