fa92ef90fc541289b823cdca8c2ed270a05c7606
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2024-04-25 15:44:18 +0200

    main2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esto es el main merge1");
+        System.out.println("Esto es el main2");
     }
 }
